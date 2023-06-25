e101166d59aa09c38e1f1bb9f9f34cf09b376113
hassu <hassu@ubuntu.myguest.virtualbox.org>
2023-06-25 04:44:44 -0700

    kuberenetes manifests, gcloud build sdk script

--- src/main/java/us/hassu/SpringApp.java
+++ src/main/java/us/hassu/SpringApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import us.hassu.service.GraphService;
-import us.hassu.spring.Secrets;
 
 @SpringBootApplication
 public class SpringApp {
@@ -15,11 +14,5 @@ public class SpringApp {
     @Bean
     GraphService graphService() {
         return new GraphService();
-    }
-
-//    @Bean
-    Secrets secrets() {
-        String secretsFile = "secret.properties";
-        return new Secrets(secretsFile);
     }
 }
